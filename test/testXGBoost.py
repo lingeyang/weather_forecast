9cfebbd096200657db97f7ae7c7476723bdb71fb
hezimo <1701210855@pku.edu.cn>
2018-09-07 00:02:35 +0800

    reconstruct

--- test/testXGBoost.py
+++ test/testXGBoost.py
@@ -10,7 +10,7 @@ data = np.array(data)
 X = data[:,0].reshape(len(data),1)
 y = data[:,1]
 
-rs=ShuffleSplit(n_splits=1,test_size=.25,random_state=0)
+rs=ShuffleSplit(n_splits=1,test_size=.1,random_state=0)
 rs.get_n_splits(X)
 
 X_train,X_test,y_train,y_test = None,None,None,None
@@ -21,7 +21,7 @@ for train_index,test_index in rs.split(X,y):
 
 xlf = xgb.XGBRegressor(max_depth=5, 
                         learning_rate=0.01, 
-                        n_estimators=10, 
+                        n_estimators=100, 
                         silent=True, 
                         objective='reg:linear', 
                         nthread=-1, 
@@ -39,5 +39,8 @@ xlf = xgb.XGBRegressor(max_depth=5,
 
 xlf.fit(X_train, y_train, eval_metric='rmse', \
         verbose = True, eval_set = [(X_test, y_test)],early_stopping_rounds=100)
+evals_result = xlf.evals_result()
+print(evals_result)
 preds = xlf.predict(X_test)
-
+print(y_test)
+print(preds)
