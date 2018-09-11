e384b0e78eac6b069ae33955ed96d77b32fc8f8b
lzk <1033731787@qq.com>
2018-09-12 00:00:36 +0800

    1

--- version_001_ignore_station/dataScrubbing.py
+++ version_001_ignore_station/dataScrubbing.py
@@ -49,15 +49,15 @@ foreTimes=37
 repeatTimes=13
 infoIndex = json.load(f)
 def dataScrub():
-    for i in range(1,11):
+    datax = []
+    datay = []
+    for name in range(1,11):
         filePath=""
-        if i<10:
-            filePath=TrainingPath+"0"+str(i)+".npy"
+        if name<10:
+            filePath=TrainingPath+"0"+str(name)+".npy"
         else:
-            filePath=TrainingPath+str(i)+".npy"
+            filePath=TrainingPath+str(name)+".npy"
         context = np.load(filePath)
-        datax = []
-        datay=[]
         for loop in range(1,len(context)-WindowSize):
             l=[]
             m=[]
@@ -67,12 +67,11 @@ def dataScrub():
                     for j in context[loop][i]:
                         l.append(j)
                 datax.append(l)
-                if  loop<len(context):
-                    for i in range(repeatTimes,foreTimes):
-                        m.append(context[loop ][i][infoIndex['t2m_obs']])
-                        m.append(context[loop ][i][infoIndex['rh2m_obs']])
-                        m.append(context[loop ][i][infoIndex['w10m_obs']])
-                        datay.append(m)
+                for i in range(repeatTimes,foreTimes):
+                    m.append(context[loop ][i][infoIndex['t2m_obs']])
+                    m.append(context[loop ][i][infoIndex['rh2m_obs']])
+                    m.append(context[loop ][i][infoIndex['w10m_obs']])
+                datay.append(m)
             else:
                 for day in range(loop,loop+WindowSize):
                     if first:
@@ -85,12 +84,11 @@ def dataScrub():
                             for j in context[day][i]:
                                 l.append(j)
                 datax.append(l)
-                if (loop+WindowSize)<len(context):
-                    for i in  range(repeatTimes,foreTimes):
-                        m.append(context[loop+WindowSize][i][infoIndex['t2m_obs']])
-                        m.append(context[loop + WindowSize][i][infoIndex['rh2m_obs']])
-                        m.append(context[loop + WindowSize][i][infoIndex['w10m_obs']])
-                    datay.append(m)
+                for i in  range(repeatTimes,foreTimes):
+                    m.append(context[loop+WindowSize][i][infoIndex['t2m_obs']])
+                    m.append(context[loop + WindowSize][i][infoIndex['rh2m_obs']])
+                    m.append(context[loop + WindowSize][i][infoIndex['w10m_obs']])
+                datay.append(m)
     np.save("datax",datax)
     np.save("datay",datay)
 
