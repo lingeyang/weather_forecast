b68156eb1eb8df126e030e25e9bd5b64d74f33bc
lzk <1033731787@qq.com>
2018-09-11 23:32:07 +0800

    dataScrubbing

--- version_001_ignore_station/dataScrubbing.py
+++ version_001_ignore_station/dataScrubbing.py
@@ -1,22 +1,59 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-
-obs_index = 29 # this index is 'psur_obs'
-
-def getFeature(trainSetPath):
-    data = np.load(trainSetPath)
-    dataLen = len(data)
-    print(data[1][0])
-    #for index in rang e(lastNDays,dataLen):
-     #   train = data[index-lastNDays:index]
-
-if __name__ == '__main__':
-    trainSetPath = '../transform_data/validation/station_90001.npy'
-    getFeature(trainSetPath)
-
-
-
+import  json
+TrainingPath=r"../transform_data/trainingset/station_900"
+WindowSize=0
+f = open(r"../util/infoIndex.json")
+foreTimes=37
+repeatTimes=13
+infoIndex = json.load(f)
+def dataScrub():
+    for i in range(1,11):
+        filePath=""
+        if i<10:
+            filePath=TrainingPath+"0"+str(i)+".npy"
+        else:
+            filePath=TrainingPath+str(i)+".npy"
+        context = np.load(filePath)
+        datax = []
+        datay=[]
+        for loop in range(1,len(context)-WindowSize):
+            l=[]
+            m=[]
+            first=True
+            if WindowSize==0:
+                for i in range(repeatTimes):
+                    for j in context[loop][i]:
+                        l.append(j)
+                datax.append(l)
+                if  loop<len(context):
+                    for i in range(repeatTimes,foreTimes):
+                        m.append(context[loop ][i][infoIndex['t2m_obs']])
+                        m.append(context[loop ][i][infoIndex['rh2m_obs']])
+                        m.append(context[loop ][i][infoIndex['w10m_obs']])
+                        datay.append(m)
+            else:
+                for day in range(loop,loop+WindowSize):
+                    if first:
+                        for i in range(foreTimes):
+                            for j in context[day][i]:
+                                l.append(j)
+                        first=False
+                    else:
+                        for i in range(repeatTimes,foreTimes):
+                            for j in context[day][i]:
+                                l.append(j)
+                datax.append(l)
+                if (loop+WindowSize)<len(context):
+                    for i in  range(repeatTimes,foreTimes):
+                        m.append(context[loop+WindowSize][i][infoIndex['t2m_obs']])
+                        m.append(context[loop + WindowSize][i][infoIndex['rh2m_obs']])
+                        m.append(context[loop + WindowSize][i][infoIndex['w10m_obs']])
+                    datay.append(m)
+    np.save("datax",datax)
+    np.save("datay",datay)
+dataScrub()
 
 
 
