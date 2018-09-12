3e93a1a2f0bd9d133c2e5bcb3cffb3e8ba989abc
S9016308 <S9016308@adc.com>
2018-09-12 13:28:17 +0800

    rebase

--- version_001_ignore_station/dataScrubbing.py
+++ version_001_ignore_station/dataScrubbing.py
@@ -42,6 +42,59 @@ def getTrainFeature(trainSetDir, nTimes):
     return X, y
 
 
+TrainingPath=r"../transform_data/trainingset/station_900"
+WindowSize=0
+f = open(r"../util/infoIndex.json")
+foreTimes=37
+repeatTimes=13
+infoIndex = json.load(f)
+def dataScrub():
+    for i in range(1,11):
+        filePath=""
+        if i<10:
+            filePath=TrainingPath+"0"+str(i)+".npy"
+        else:
+            filePath=TrainingPath+str(i)+".npy"
+        context = np.load(filePath)
+        datax = []
+        datay=[]
+        for loop in range(1,len(context)-WindowSize):
+            l=[]
+            m=[]
+            first=True
+            if WindowSize==0:
+                for i in range(repeatTimes):
+                    for j in context[loop][i]:
+                        l.append(j)
+                datax.append(l)
+                if  loop<len(context):
+                    for i in range(repeatTimes,foreTimes):
+                        m.append(context[loop ][i][infoIndex['t2m_obs']])
+                        m.append(context[loop ][i][infoIndex['rh2m_obs']])
+                        m.append(context[loop ][i][infoIndex['w10m_obs']])
+                        datay.append(m)
+            else:
+                for day in range(loop,loop+WindowSize):
+                    if first:
+                        for i in range(foreTimes):
+                            for j in context[day][i]:
+                                l.append(j)
+                        first=False
+                    else:
+                        for i in range(repeatTimes,foreTimes):
+                            for j in context[day][i]:
+                                l.append(j)
+                datax.append(l)
+                if (loop+WindowSize)<len(context):
+                    for i in  range(repeatTimes,foreTimes):
+                        m.append(context[loop+WindowSize][i][infoIndex['t2m_obs']])
+                        m.append(context[loop + WindowSize][i][infoIndex['rh2m_obs']])
+                        m.append(context[loop + WindowSize][i][infoIndex['w10m_obs']])
+                    datay.append(m)
+    np.save("datax",datax)
+    np.save("datay",datay)
+
+
 if __name__ == '__main__':
     trainSetPath = '../transform_data/trainingset'
     getTrainFeature(trainSetPath,24)
