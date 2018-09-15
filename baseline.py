af8cceffb4dd82e3bc0c1f1d42c4664f031dfa85
hezimo <1701210855@pku.edu.cn>
2018-09-15 08:53:49 +0800

    version 1.0 failed

--- baseline.py
+++ baseline.py
@@ -17,19 +17,19 @@ def metricsGetCost(dirPath, nTimes):
     imp = Imputer(missing_values=-9999., strategy='mean', axis=0)
     
     files = os.listdir(dirPath)
-    for file in files[:1]:
+    for file in files:
         filePath = dirPath + os.sep + file
         data = np.load(filePath)[-1][37-nTimes:]
         data = imp.fit_transform(data)
         y = data[:,[infoIndex['t2m_obs'], infoIndex['rh2m_obs'], infoIndex['w10m_obs']]]
         y_pred = data[:,[infoIndex['t2m_M'], infoIndex['rh2m_M'], infoIndex['w10m_M']]]
         tmp_cost = metrics.mean_squared_error(y, y_pred) * nTimes
-        bcost = metrics.mean_squared_error(y.flatten(), y_pred.flatten())
+        #bcost = metrics.mean_squared_error(y.flatten(), y_pred.flatten())
         cost += tmp_cost
-        print(y, y_pred)
-        print('baseline cost : %.4f' % (bcost))
+        #print(y, y_pred)
+        #print('baseline cost : %.4f' % (bcost))
     cost = (cost/(nTimes*len(files))) ** 0.5
-    #print(cost)
+    print('baseline cost : %.4f' % (cost))
 
 if __name__ == '__main__':
     dirPath = './transform_data/validation'
--- version_001_ignore_station/train.py
+++ version_001_ignore_station/train.py
@@ -1,90 +1,63 @@
 # -*- coding: utf-8 -*-
 
 import dataScrubbing as ds
-from sklearn.multioutput import MultiOutputRegressor
+#from sklearn.multioutput import MultiOutputRegressor
 from xgboost import XGBRegressor
 from xgboost import XGBModel
 from sklearn import metrics
 from sklearn.preprocessing import Imputer
 import numpy as np
 import json
+import matplotlib.pyplot as plt
+from sklearn import linear_model
 
-
-def multiXGBoost(max_depth=4, learning_rate=0.1, n_estimators=100):
-    X_train = np.load('X.npy')
-    y_train = np.load('y.npy')
-    y_train = y_train[:,5]
-    
+def modelXGBoost(max_depth=100, learning_rate=0.3, n_estimators=50):
+    infoBoundPath = '../util/infoBound.json'
+    with open(infoBoundPath,'r') as f:
+        infoBound = json.load(f)
+    index = 30
+    lr = linear_model.LinearRegression()
+    infoIndexPath = '../util/infoIndex.json'
+    validationPath = '../transform_data/validation'
+    trainSetPath = '../transform_data/trainingset'
+    x_train, y_train = ds.__getTrainFeature(trainSetPath, infoIndexPath)
+    y_train = y_train[:,index]
+    x_test, y_test = ds.__getTrainFeature(validationPath, infoIndexPath)
+    y_test = y_test[:,index]
+    #x_test = x_test[-1][np.newaxis,:]
+    #y_test = [y_test[-1]]
+    #print(x_test.shape,y_test.shape)
+    '''
+    #max_depth = int(infoBound['t2m_obs'][1] ** 0.5)
     xlf = XGBRegressor(max_depth=max_depth, 
                         learning_rate=learning_rate, 
                         n_estimators=n_estimators, 
                         objective='reg:linear', 
-                        n_jobs=64,
-                        subsample=0.85, 
-                        colsample_bytree=0.85, 
-                        colsample_bylevel=1.0, 
+                        subsample=1, 
+                        colsample_bytree=1, 
+                        colsample_bylevel=1, 
                         reg_alpha=0, 
-                        reg_lambda=100, 
-                        scale_pos_weight=1, 
-                        random_state=1000)
+                        reg_lambda=1, 
+                        scale_pos_weight=1,
+                        missing=-9999.)
     
-    #clf = MultiOutputRegressor(xlf)
-    #clf.fit(X, y)
-    xlf.fit(X_train, y_train)
-    return xlf
-    
-def train(X_test, y_test):
-    
-    X_train = np.load('X.npy')
-    y_train = np.load('y.npy')
-    y_train = y_train[:,24]
-    param_dist = {'objective':'reg:linear', \
-                  'max_depth':4, \
-                  'eta':0.1, \
-                  'colsample_bytree':0.8, \
-                  'n_estimators':1, \
-                  'lambda':100
-                  }
-    clf = XGBModel(**param_dist)
-    clf.fit(X_train, y_train,
-        eval_set=[(X_train, y_train),(X_test, y_test)],
+    xlf.fit(x_train, y_train,
+        eval_set=[(x_train, y_train),(x_test, y_test)],
         eval_metric='rmse',
         verbose=True)
-    return clf
+    y_pred = xlf.predict(x_test)
+    '''
+    lr.fit(x_train, y_train)
+    y_pred = lr.predict(x_test)
+    pt, = plt.plot(range(len(y_test)),y_test)
+    pp, = plt.plot(range(len(y_pred)),y_pred)
+    plt.legend([pt,pp],['y_test','y_pred'],loc='upper right')
+    plt.show()
+    #print(y_pred[:10])
+    #print(y_test[:10])
+    #return xlf
+    
 
 if __name__ == '__main__':
-    infoIndexPath = '../util/infoIndex.json'
-    validationPath = '../transform_data/validation'
-    x_test, y_test = ds.__getTrainFeature(validationPath,infoIndexPath)
-
-    '''
-    with open(infoIndexPath, 'r') as f:
-        infoIndex = json.load(f)
-    imp = Imputer(missing_values=-9999.)
-    validationPath = '../transform_data/validation/station_90001.npy'
-    
-    vdata = np.load(validationPath)[-1]
-    vdata = imp.fit_transform(vdata)
-    X = vdata[13:][:,:29] \
-    [:,[infoIndex['t2m_M'], infoIndex['rh2m_M'], infoIndex['w10m_M']]].flatten()[np.newaxis,:]
-    
-    #X = np.concatenate((x1,x2))[np.newaxis,:]
-    y = vdata[13:, \
-               [infoIndex['t2m_obs'], infoIndex['rh2m_obs'], infoIndex['w10m_obs']]]
-    y_train = np.load('y.npy')
-    y = vdata[13,infoIndex['t2m_obs']]
-    y = y.flatten()
-    #print(X.shape, y.shape)
-    '''
-    x_test = x_test[-1][np.newaxis,:]
-    y_test = [y_test[:,23][-1]]
-    clf = train(x_test, y_test)
-    #print(clf.evals_result())
-    
-    #y_pred = clf.predict(x_test)
-    #print(y_test)
-    #print(y_pred)
-    #cost = metrics.mean_squared_error(y, y_pred)
-    #print('model cost : %.4f' % cost)
-    
+    modelXGBoost()
 
